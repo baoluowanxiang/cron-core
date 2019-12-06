c6428635bf75cc6255bdbc89f0931720185485cb
zhouzhe <zhouzhe@xin.com>
2019-12-06 20:02:59 +0800

    1、更新tcp服务

--- service/tcp.go
+++ service/tcp.go
@@ -3,16 +3,35 @@ package service
 import (
 	"bufio"
 	"crontab/base"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"strings"
+	"sync"
 )
 
 type TcpService struct {
-	opt *base.ClientOpt
+	opt       *base.ClientOpt
+	connMutex sync.Mutex
 }
+
+// 连接服务hash表
+type connMap map[string][]*net.Conn
+
+// client 注册请求
+type ClientRegisterRequest struct {
+	ServiceName string `json:"service_name"`
+	Token       string `json:"token"`
+}
+
+// 连接映射表
+var connHashMap = connMap{}
+
+// 连接通道
+var connChannel = make(chan *net.Conn)
 
 func (t *TcpService) Start() error {
 	go func() {
@@ -26,6 +45,7 @@ func (t *TcpService) Start() error {
 
 func (t *TcpService) SetOpt(opt *base.ClientOpt) {
 	t.opt = opt
+	t.connMutex = sync.Mutex{}
 }
 
 func (t *TcpService) Send(data base.JobData) {
@@ -70,22 +90,48 @@ func (t *TcpService) waitForConnection(listener net.Listener) {
 
 func (t *TcpService) authenticate(conn net.Conn) {
 	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
-	defer func() {
-		_ = conn.Close()
-	}()
 	for {
-		cmd, err := rw.ReadString('\n')
-		cmd = strings.Trim(cmd, "\n ")
-		log.Print(cmd)
-		switch {
-		case err == io.EOF:
-			fmt.Println("客户端关闭连接.")
-			return
-		case err != nil:
-			fmt.Println("连接中断")
-			return
+		authString, err := rw.ReadString('\n')
+		authString = strings.Trim(authString, "\n ")
+		if data, errc := t.validateConnection(conn, authString); errc == nil {
+			switch {
+			case err == io.EOF: //客户端关闭了连接
+				_ = conn.Close()
+				return
+			case err != nil: //连接断开了
+				_ = conn.Close()
+				return
+			}
+			t.saveConnection(conn, data)
+		} else {
+			_, _ = conn.Write([]byte("unauthenticate connection"))
+			_ = conn.Close()
 		}
 	}
+}
+
+func (t *TcpService) validateConnection(conn net.Conn, str string) (*ClientRegisterRequest, error) {
+	request := &ClientRegisterRequest{}
+	err := json.Unmarshal([]byte(str), request)
+	if err != nil {
+		return nil, errors.New("数据结构异常")
+	}
+	if request.ServiceName == "" || request.Token == "" {
+		return nil, errors.New("参数异常")
+	}
+	return request, nil
+}
+
+func (t *TcpService) saveConnection(conn net.Conn, req *ClientRegisterRequest) {
+	var list []*net.Conn
+	t.connMutex.Lock()
+	list, ok := connHashMap[req.ServiceName]
+	if !ok {
+		list = []*net.Conn{}
+	}
+	list = append(list, &conn)
+	connHashMap[req.ServiceName] = list
+	t.connMutex.Unlock()
 }
 
 func (t *TcpService) loop() {
