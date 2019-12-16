b0d7343aa96705749443bf9cb82954e6525630f6
zhouzhe1157 <zhouzhe1157@qq.com>
2019-12-16 21:35:13 +0800

    1、 调整tcp服务

--- orm/db.go
+++ orm/db.go
@@ -8,9 +8,11 @@ import (
 var GlobalDb *gorm.DB
 
 func init() {
+	/**
 	db, err := gorm.Open("mysql", "cron:admin@(10.70.30.26:3306)/cron?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect database" + err.Error())
 	}
 	GlobalDb = db
+	**/
 }
